886cbf2a3da6bd6344842e9c240c9e7b920690f7
Ashish Gulab <ashishgulab.gulab@gmail.com>
2023-05-18 11:53:33 +0530

    Add files via upload

---
+++ hello-world-app/src/index.js
@@ -0,0 +1,10 @@
+// This is the older format of writing the javascript
+// var React=require('react');
+// var ReactDom=require('react-dom');
+
+// Now we use the Bable that is import when we import the 'react'.
+// Babel is the compiler that convert the modern javascript according to the brower running javascript. So we will prefer the 'import' method to import the react etc.
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(<h1>Hello World !!!</h1>, document.getElementById('root'));

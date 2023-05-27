a15d60359e5a978ca120de51a5cc38a9dfa81ecb
Ashish Gulab <ashishgulab.gulab@gmail.com>
2023-05-27 11:39:10 +0530

    Add files via upload

---
+++ react-app6/src/Components/Movies.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+export default function Movies(){
+    return(
+        <ol>
+            <li>Avengers The End Game</li>
+            <li>Avatar</li>
+            <li>Dangal</li>
+            <li>Damdar Khiladi</li>
+            <li>Super Khiladi</li>
+        </ol>
+    )
+}
---
+++ react-app6/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Heading from './Components/Heading'
+import Movies from './Components/Movies';
+
+ReactDom.render(<><Heading/>,<Movies/></>,document.getElementById('root'));

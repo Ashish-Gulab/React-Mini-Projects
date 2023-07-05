65c6a51aba46eaf84c52430a3f1e723970c27858
Ashish Gulab <ashishgulab.gulab@gmail.com>
2023-07-05 13:21:54 +0530

    Update index.js

--- react-app3/src/index.js
+++ react-app3/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
 import './index.css';
 
 const name='Ashish Gulab';
@@ -7,12 +7,12 @@ let image1='https://picsum.photos/200/300';
 let image2='https://picsum.photos/250/300';
 let image3='https://picsum.photos/300/300';
 
-ReactDom.render(<>
+const root=ReactDom.createRoot(document.getElementById('root'));
+root.render(<>
   <h1>My name is {name}</h1>
   <div className="images">
-    <img src={image1}/>
-    <img src={image2}/>
-    <img src={image3}/>
+    <img src={image1} alt="normal_view"/>
+    <img src={image2} alt="normal_view"/>
+    <img src={image3} alt="normal_view"/>
   </div>
-  </>,
-  document.getElementById('root'));
+  </>);

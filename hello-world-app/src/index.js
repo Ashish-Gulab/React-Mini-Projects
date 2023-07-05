6425d5b6c1fd20e561b2860f9c72739ef02fb5d6
Ashish Gulab <ashishgulab.gulab@gmail.com>
2023-07-05 13:18:57 +0530

    Update index.js

--- hello-world-app/src/index.js
+++ hello-world-app/src/index.js
@@ -5,6 +5,7 @@
 // Now we use the Bable that is import when we import the 'react'.
 // Babel is the compiler that convert the modern javascript according to the brower running javascript. So we will prefer the 'import' method to import the react etc.
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 
-ReactDom.render(<h1>Hello World !!!</h1>, document.getElementById('root'));
+const root=ReactDom.createRoot(document.getElementById('root'));
+root.render(<h1>Hello World !!!</h1>);

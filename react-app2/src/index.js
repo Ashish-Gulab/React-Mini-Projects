2c84e570b7cff939ab624ae100a9faac397394f7
Ashish Gulab <ashishgulab.gulab@gmail.com>
2023-07-05 13:20:38 +0530

    Update index.js

--- react-app2/src/index.js
+++ react-app2/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 
 const name='Ashish Gulab';
 let date=new Date().toLocaleDateString();
 let time=new Date().toLocaleTimeString();
 
-ReactDom.render(<>
-  <h1>My name is {name}</h1>
-  <p>Today's Date is {date}</p>
-  <p>Current time is {time}</p>
-  </>,
-  document.getElementById('root'));
+const root=ReactDom.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <h1>My name is {name}</h1>
+    <p>Today's Date is {date}</p>
+    <p>Current time is {time}</p>
+  </>);
